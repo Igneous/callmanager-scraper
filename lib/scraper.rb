303989cf1f11167100773ecb45a23841c7498013
Bucky Wolfe <bwolfe@franklinamerican.com>
2013-07-31 21:06:48 -0500

    Now a Web Service, also gets device attrs

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,43 +1,48 @@
 require 'watir-webdriver'
 require 'nokogiri'
+require 'headless'
 
 class ShitscoScraper 
 
-  # Creates a new instance of ShitscoScraper, then associates the Watir session
-  # to an instance variable, "@browser".
+  # e.g ShitscoScraper.new :cucm_host => "172.20.1.15", :browser => :chrome
   def initialize(options = {})
-    unless options[:browser]
-      @browser = Watir::Browser.new :firefox
-    else
-      @browser = Watir::Browser.new options[:browser]
-    end
+    @hostname = options[:cucm_host]
+    @browser  = Watir::Browser.new options[:browser]
   end
 
+  # primarily for the convienience of debugging.. but also nice for .close and .goto
   def method_missing(meth, *args, &block)
     @browser.send meth, *args, &block 
   end
 
-  # Logs into the cucm form.
-  # Options Hash requires :url (to showHome.do), :username, and :password.
+  # Logs into the cucm.
+  # Options Hash requires :username, and :password.
   def login(options = {})
-    @browser.goto options[:url]
+    @browser.goto 'https://' + @hostname + '/ccmadmin/showHome.do'
     @browser.text_field(:name => 'j_username').set options[:username]
     @browser.text_field(:name => 'j_password').set options[:password]
     @browser.button(:value => 'Login').click
     raise 'Unable to Login' unless @browser.text.include? 'Logout'
   end
 
+  # Private method, so it doesn't get confused with list_phones..
+  # Dumps table html of the result of searching "SEP" on the phone page.
   def get_phone_table
     @browser.goto @browser.link(:text => 'Phone').href
     @browser.text_field(:id => 'searchString0').set 'SEP'
     @browser.button(:value => 'Find').click
+    # After we've made the initial request and gotten the results,
+    # set the rows returned to an arbitrarily large number, so we can
+    # display all the results on a single page.
     @browser.goto @browser.url + '&rowsPerPage=9999'
 
     return @browser.table(:summary => 'Find List Table Result').html
-  end
+  end; private :get_phone_table
 
+  # Calls get_phone_table, then parses the html and returns an array
+  # of hashes (one hash per device).
   def list_phones
-    phones = []
+    phones = [] # We're going to push to this array momentarily.
     phonetable = Nokogiri::HTML(get_phone_table)
     
     # For each table row.
@@ -46,17 +51,24 @@ class ShitscoScraper
       # all of the elements matching //td from the previous doc..
       newrow = Nokogiri::HTML(row.to_s)
 
-      # for each row, run "row.content" and map them to an array
+      # for all cells in the row, run "newrow.content" and map them to an array
       cols = newrow.xpath('//td').map(&:content)
 
       # if the status is registered, stick the phonehash in the phone array
       # also, special xpaths that aren't just plaintext.. might as well do it
       # here rather than testing for nil (again) outside of this..
       if not cols[6].nil? and cols[6].start_with? 'Registered'
+        # Get the link destination of the column with the icon.
+        uuid  = newrow.xpath('//td[starts-with(@id, "icon")]/a').attr('href').content
+        # Assuming uuid is defined, grab UUID from the 'key=' get parameter.
+        uuid &&= uuid.split('key=').last 
+        # Scrape the alt-text of the phone icon to determine the model name.
+        model = newrow.xpath('//td[starts-with(@id, "icon")]/a/img').attr('alt').content
+        # Push all of this shit into the phones array.
         phones << {
           :mac    => cols[2],
-          :uuid   => newrow.xpath('//td[starts-with(@id, "icon")]/a').attr('href').content,
-          :model  => newrow.xpath('//td[starts-with(@id, "icon")]/a/img').attr('alt').content,
+          :uuid   => uuid,
+          :model  => model,
           :dscr   => cols[3],
           :status => cols[6],
           :ipaddr => cols[7]
@@ -67,9 +79,24 @@ class ShitscoScraper
     return phones
   end
 
-  # FIX ME.. 
-  def get_username(uuid)
-    todo
+  def get_device_attributes(uuid)
+    @browser.goto 'https://' + @hostname + '/ccmadmin/gendeviceEdit.do?key=' + uuid
+    
+    user_list = Nokogiri::HTML(@browser.select_list(:name => 'fkenduser').html)
+    username = user_list.xpath('//option[@selected]').first
+    username &&= username.content # if username not nil, set to username.content
+
+    # Grab the "Associations Information" table on the lefthand side of the page
+    assoc_info = Nokogiri::HTML(@browser.table(:summary => 'Associations').html)
+    # map the second column of each row to an array, if and only if it is a line
+    # i.e. extract 2600 from a string like "Line [1] - 2600 in Some Partition"
+    dirnums = assoc_info.xpath('//td').map(&:content)
+                    .select { |content| content =~ /Line \[\d\] - \d+ in/ }
+                    .map    { |dn| dn.split('-').last
+                                     .split('in').first
+                                     .strip! }
+
+    return { :uuid => uuid, :user => username, :dn => dirnums }
   end
 
 end
--- shitscraper.rb
+++ shitscraper.rb
@@ -3,20 +3,78 @@ path = File.expand_path $0
 path = File.dirname(path)
 require "#{path}/lib/scraper.rb"
 require 'syck'
+require 'headless'
+require 'sinatra'
+require 'yaml'
+require 'pry'
 
-LOGIN_URL = "https://your.com/ccmadmin/showHome.do"
-USERNAME  = "replaceme"
-PASSWORD  = "replaceme"
+# -- svc settings -- #
+set :port, 8081
+set :bind, '0.0.0.0'
+set :environment, :production
+HOSTNAME = "cucm01.your.com"
+USERNAME  = "readonly-acct"
+PASSWORD  = "hackme"
 
-if __FILE__ == $PROGRAM_NAME
+# scrape() starts up an Xvfb server via the headless gem,
+# then spawns an instance of ShitscoScraper inside of it,
+# logs in, and then yields to whatever block it was given.
+# (and ensures that it tears down xvfb/chrome when done)
+#
+# I'd like to put this in ShitscoScraper somehow.. but
+# I'm not not entirely sure the best way.. For the time
+# being, it's here.
+def scrape(&block)
+    headless = Headless.new
+    headless.start
+    begin
+      scraper = ShitscoScraper.new :cucm_host => HOSTNAME,
+                                   :browser   => :chrome
 
-  scraper = ShitscoScraper.new
-  at_exit { scraper.close if scraper }
+      scraper.login :username => USERNAME,
+            		    :password => PASSWORD
 
-  scraper.login :url => LOGIN_URL,
-                :username => USERNAME,
-                :password => PASSWORD
+      yield(scraper)
+    ensure
+      scraper.close
+      headless.destroy
+    end
+end
 
-  puts scraper.list_phones.to_yaml
+# No parameters required.. /phones/gets returns a yaml doc
+# of all the phones.
+#
+# Returns an array of hashes, each hash representing a phone.
+# Each hash contains the keys:
+# :mac    (The MAC address of the device),
+# :uuid   (The UUID of the device, according to CUCM),
+# :model  (The (full) Model name of the device),
+# :dscr   (Any Description information for the device),
+# :status (Registration status of the device),
+# :ipaddr (IP Address of the device)
+get '/phones/get' do
+    scrape do |scraper|
+      return scraper.list_phones.to_yaml
+    end
+end
 
+# device_attributes for the time being needs to be POSTed a
+# yaml array called 'list'.. It should not be form/multipart
+# encoded, just plaintext/urlencoded is all that's required.
+#
+# Returns an array of hashes; the hashes contain the keys:
+# :uuid (the original uuid passed in),
+# :user (the owner of the device assoc'd with the uuid),
+# :dn   (an array of Extens assoc'd with the device)
+post '/device_attributes/get' do
+  scrape do |scraper|
+    devices = []
+    uuidlist = YAML.load(params[:list])
+
+    uuidlist.each do |uuid|
+      devices.push scraper.get_device_attributes(uuid)
+    end
+
+    return devices.to_yaml
+  end
 end
